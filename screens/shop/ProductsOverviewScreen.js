776a0604d33507feab95e3000d9993e148dbde55
Chris Paterson <chrispaterson22@gmail.com>
2021-03-17 16:08:44 +0000

    Basic project setup, ProductsOverviewScreen & Redux

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+
+const ProductsOverviewScreen = props => {
+	return (
+		<FlatList>
+			<Text></Text>
+		</FlatList>
+	)
+}
+
+export default ProductsOverviewScreen
+
+const styles = StyleSheet.create({})
