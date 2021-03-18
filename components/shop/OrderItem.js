c3e852bb7728d361a3d343dcd3d081789d727d2d
Chris Paterson <chrispaterson22@gmail.com>
2021-03-18 21:43:21 +0000

    show details function on orders screen

--- components/shop/OrderItem.js
+++ components/shop/OrderItem.js
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View, Button } from 'react-native'
 import Colors from '../../constants/Colors'
 
 import CartItem from './CartItem'
 
 const OrderItem = props => {
+	const [showDetails, setShowDetails] = useState(false)
+
 	return (
 		<View style={styles.orderItem}>
 			<View style={styles.summary}>
 				<Text style={styles.totalAmount}>${props.amount.toFixed(2)}</Text>
 				<Text style={styles.date}>{props.date}</Text>
 			</View>
-			<Button color={Colors.primary} title='Show Details' />
+			<Button
+				color={Colors.primary}
+				title={showDetails ? 'Hide Details' : 'Show Details'}
+				onPress={() => {
+					setShowDetails(prevState => !prevState)
+				}}
+			/>
+			{showDetails && (
+				<View style={styles.detailItems}>
+					{props.items.map(cartItem => (
+						<CartItem
+							key={cartItem.productId}
+							quantity={cartItem.quantity}
+							amount={cartItem.sum}
+							title={cartItem.productTitle}
+						/>
+					))}
+				</View>
+			)}
 		</View>
 	)
 }
@@ -46,5 +66,8 @@ const styles = StyleSheet.create({
 		fontFamily: 'open-sans',
 		fontSize: 16,
 		color: '#888',
+	},
+	detailItems: {
+		width: '100%',
 	},
 })
