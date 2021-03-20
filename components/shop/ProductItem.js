75efd37482c7f67b5b8460104b81ea776057aced
Chris Paterson <chrispaterson22@gmail.com>
2021-03-20 16:54:09 +0000

    Gen app improvements

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -10,7 +10,7 @@ import {
 	Platform,
 } from 'react-native'
 
-import Colors from '../../constants/Colors'
+import Card from '../UI/Card'
 
 const ProductItem = props => {
 	let TouchableComp = TouchableOpacity
@@ -20,7 +20,7 @@ const ProductItem = props => {
 	}
 
 	return (
-		<View style={styles.product}>
+		<Card style={styles.product}>
 			<View style={styles.touchable}>
 				<TouchableComp onPress={props.onSelect} useForeground>
 					<View>
@@ -35,7 +35,7 @@ const ProductItem = props => {
 					</View>
 				</TouchableComp>
 			</View>
-		</View>
+		</Card>
 	)
 }
 
@@ -43,13 +43,6 @@ export default ProductItem
 
 const styles = StyleSheet.create({
 	product: {
-		shadowColor: 'black',
-		shadowOpacity: 0.26,
-		shadowOffset: { width: 0, height: 2 },
-		shadowRadius: 8,
-		elevation: 5,
-		borderRadius: 10,
-		backgroundColor: '#fff',
 		height: 300,
 		margin: 20,
 	},
--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -6,6 +6,7 @@ import {
 	Button,
 	FlatList,
 	Platform,
+	Alert,
 } from 'react-native'
 import { useSelector, useDispatch } from 'react-redux'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
@@ -21,6 +22,19 @@ const UserProductsScreen = props => {
 
 	const editProductHandler = id => {
 		props.navigation.navigate('EditProduct', { productId: id })
+	}
+
+	const deleteHandler = id => {
+		Alert.alert('Are you sure?', 'Do you really want to delete this item?', [
+			{ text: 'No', style: 'default' },
+			{
+				text: 'Yes',
+				style: 'destructive',
+				onPress: () => {
+					dispatch(productsActions.deleteProduct(id))
+				},
+			},
+		])
 	}
 
 	return (
@@ -45,9 +59,7 @@ const UserProductsScreen = props => {
 					<Button
 						color={Colors.primary}
 						title='Delete'
-						onPress={() => {
-							dispatch(productsActions.deleteProduct(itemData.item.id))
-						}}
+						onPress={deleteHandler.bind(this, itemData.item.id)}
 					/>
 				</ProductItem>
 			)}
