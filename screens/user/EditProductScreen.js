c6601e0bd6f1078220cd48cb39d63206c6e27873
Chris Paterson <chrispaterson22@gmail.com>
2021-04-03 14:52:38 +0100

    setting screen options dynamically

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -109,7 +109,19 @@ const EditProductScreen = props => {
 	}, [dispatch, prodId, formState])
 
 	useEffect(() => {
-		props.navigation.setParams({ submit: submitHandler })
+		props.navigation.setOptions({
+			headerRight: () => (
+				<HeaderButtons HeaderButtonComponent={HeaderButton}>
+					<Item
+						title='Save'
+						iconName={
+							Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'
+						}
+						onPress={submitHandler}
+					/>
+				</HeaderButtons>
+			),
+		})
 	}, [submitHandler])
 
 	const inputChangeHandler = useCallback(
@@ -195,22 +207,10 @@ const EditProductScreen = props => {
 }
 
 export const screenOptions = navData => {
-	const submitFn = navData.route.params ? navData.route.params.submit : null
 	const routeParams = navData.route.params ? navData.route.params : {}
 
 	return {
 		headerTitle: routeParams.productId ? 'Edit Product' : 'Add Product',
-		headerRight: () => (
-			<HeaderButtons HeaderButtonComponent={HeaderButton}>
-				<Item
-					title='Save'
-					iconName={
-						Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'
-					}
-					onPress={submitFn}
-				/>
-			</HeaderButtons>
-		),
 	}
 }
 
