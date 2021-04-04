252ac2f66f93758db29248ae5a2b4ac2cbb7667e
Chris Paterson <chrispaterson22@gmail.com>
2021-04-04 16:14:36 +0100

    adding push token to user created product

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,4 +1,6 @@
 import Product from '../../models/product'
+import * as Notifications from 'expo-notifications'
+import * as Permissions from 'expo-permissions'
 
 export const DELETE_PRODUCT = 'DELETE_PRODUCT'
 export const CREATE_PRODUCT = 'CREATE_PRODUCT'
@@ -66,6 +68,17 @@ export const deleteProduct = productId => {
 export const createProduct = (title, description, imageUrl, price) => {
 	return async (dispatch, getState) => {
 		// any async code you want!
+		let pushToken
+		let statusObj = await Permissions.getAsync(Permissions.NOTIFICATIONS)
+		if (statusObj.status !== 'granted') {
+			statusObj = await Permissions.askAsync(Permissions.NOTIFICATIONS)
+		}
+		if (statusObj.status !== 'granted') {
+			pushToken = null
+		} else {
+			pushToken = (await Notifications.getExpoPushTokenAsync()).data
+		}
+
 		const token = getState().auth.token
 		const userId = getState().auth.userId
 		const response = await fetch(
@@ -81,6 +94,7 @@ export const createProduct = (title, description, imageUrl, price) => {
 					imageUrl,
 					price,
 					ownerId: userId,
+					ownerPushToken: pushToken,
 				}),
 			}
 		)
